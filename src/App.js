d3d6bb32039f1654e6a7d16fefd77bffd356567e
Josh Zucker <zuckerje@gmail.com>
2017-10-14 03:20:01 -0400

    merge

--- src/App.js
+++ src/App.js
@@ -20,9 +20,14 @@ class App extends Component {
         '0x00000002',
         '0x00000003',
         '0x00000004',
-      ]
-
+      ],
+      waitlistInstance: null,
+      currentPosition: null
     }
+
+    this.getCurrentList = this.getCurrentList.bind(this)
+    this.getCurrentPosition = this.getCurrentPosition.bind(this)
+    this.addMeToList = this.addMeToList.bind(this)
   }
 
   componentWillMount () {
@@ -36,13 +41,12 @@ class App extends Component {
         })
 
         // Instantiate contract once web3 provided.
-        // this.instantiateContract()
+        this.instantiateContract()
       })
       .catch(() => {
         console.log('Error finding web3.')
       })
   }
-
   instantiateContract () {
     /*
      * SMART CONTRACT EXAMPLE
@@ -55,70 +59,62 @@ class App extends Component {
     const waitlist = contract(WaitlistContract)
     waitlist.setProvider(this.state.web3.currentProvider)
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var waitlistInstance
-
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
       waitlist.deployed().then((instance) => {
-        waitlistInstance = instance
-
-        return waitlistInstance.addUserToList({from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return waitlistInstance.getWaitingList.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-
-        return this.setState({storageValue: JSON.stringify(result)})
-
+        this.setState(prevState => ({...prevState, waitlistInstance: instance}))
       })
     })
   }
 
   getNetworkId () {
-
-    return <p>Hi</p>
-
     return this.state.web3.version.getNetwork((err, netId) => {
       return <p>{netId}</p>
     })
   }
 
-  render () {
+  getCurrentList () {
+    this.state.waitlistInstance.getWaitingList().then(list => {
+      this.setState(prevState => ({...prevState, waitingList: list}))
+    })
+  }
 
-    if (this.state.web3) {
+  getCurrentPosition () {
+    this.state.waitlistInstance.getCurrent().then(position => {
+      console.log('position: ' + position)
+      this.setState(prevState => ({...prevState, currentPosition: position}))
+    })
+  }
 
-      this.state.web3.eth.getTransactionReceipt('0x0e6cbf93e9fb5eca6dfb0fe43e3c7ef03d77b72265d3441953d53436ad8241f8', (err, r) => {
-        console.log(r)
-      })
+  addMeToList () {
+    // console.log(this.state.web3.eth.accounts)
+    this.state.waitlistInstance.addToWaitingList({ from: this.state.web3.eth.accounts[0] }).then(res => {
+      console.log(res)
+    })
+  }
 
-    }
+  render() {
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
           <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
         </nav>
-
-
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-
+              <h2>Waiting List</h2>
               <ul>
                 {_.map(this.state.waitingList, (waitingList) => {
                   return <li>{waitingList}</li>
                 })}
               </ul>
-
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+              <h2>Current Position</h2>
+              <div>{JSON.stringify(this.state.currentPosition)}</div>
               {this.state.web3 ? this.getNetworkId() : 'Loading'}
-
+              {this.state.waitlistInstance ? 'Got instance' : 'No instance'}
+              <button onClick={this.addMeToList}>Add me to list</button>
+              <button onClick={this.getCurrentList}>Get current list</button>
+              <button onClick={this.getCurrentPosition}>Update current position</button>
             </div>
           </div>
         </main>
